43b08e380ef1d21e60ae7009f647be8eaca0af53
Renaud Racinet <renaud.racinet@estiam.com>
2019-10-24 15:21:40 +0200

    feat(layout): add template
    add HeaderComponent
    add MainComponent
    add FooterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutTemplateComponent } from './template/layout-template/layout-template.component';
+import { LayoutHeaderComponent } from './template/layout-header/layout-header.component';
+import { LayoutMainComponent } from './template/layout-main/layout-main.component';
+import { LayoutFooterComponent } from './template/layout-footer/layout-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutTemplateComponent,
+    LayoutHeaderComponent,
+    LayoutMainComponent,
+    LayoutFooterComponent
   ],
   imports: [
     BrowserModule,
