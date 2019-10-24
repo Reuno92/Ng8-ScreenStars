40519b8f97f9f6a8729bb8f57fc8561c1bf7485a
Renaud Racinet <renaud.racinet@estiam.com>
2019-10-24 16:43:15 +0200

    feat(route): Create route for template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Template Component
+import {LayoutTemplateComponent} from "./template/layout-template/layout-template.component";
 
-const routes: Routes = [];
+// Route Component
+import {HomeComponent} from "./components/home/home.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutTemplateComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// App Component
 import { LayoutTemplateComponent } from './template/layout-template/layout-template.component';
-import { LayoutHeaderComponent } from './template/layout-header/layout-header.component';
-import { LayoutMainComponent } from './template/layout-main/layout-main.component';
-import { LayoutFooterComponent } from './template/layout-footer/layout-footer.component';
+import { LayoutHeaderComponent } from './template/layout-template/layout-header/layout-header.component';
+import { LayoutMainComponent } from './template/layout-template/layout-main/layout-main.component';
+import { LayoutFooterComponent } from './template/layout-template/layout-footer/layout-footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const LAYOUT_COMPONENTS = [
+  LayoutTemplateComponent,
+  LayoutHeaderComponent,
+  LayoutMainComponent,
+  LayoutFooterComponent,
+];
+
+const APP_COMPONENTS = [
+  HomeComponent,
+  NotFoundComponent
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutTemplateComponent,
-    LayoutHeaderComponent,
-    LayoutMainComponent,
-    LayoutFooterComponent
+    ...LAYOUT_COMPONENTS,
+    ...APP_COMPONENTS
   ],
   imports: [
     BrowserModule,
