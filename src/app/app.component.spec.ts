daa8049523651adba4e8d2360badd55a28f8d29a
Renaud Racinet <renaud.racinet@estiam.com>
2019-10-24 18:12:30 +0200

    test(AppComponent): commencing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,54 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, getTestBed, inject, TestBed} from '@angular/core/testing';
+import {RouterTestingModule, SpyNgModuleFactoryLoader} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {HomeComponent} from "./components/home/home.component";
+import {Router} from "@angular/router";
+import {Location, LocationStrategy} from "@angular/common";
+import {MockLocationStrategy, SpyLocation} from "@angular/common/testing";
+import {NgModuleFactory} from "@angular/core";
 
 describe('AppComponent', () => {
+
+  let location, router;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([
+          {
+            path: '', component: HomeComponent
+          }
+        ])
       ],
       declarations: [
-        AppComponent
+        AppComponent, HomeComponent
       ],
+      providers: [
+        {
+          provide: Location, useClass: SpyLocation
+        },
+        {
+          provide: LocationStrategy, useClass: MockLocationStrategy
+        },
+        {
+          provide: NgModuleFactory, useClass: SpyNgModuleFactoryLoader
+        }
+      ]
     }).compileComponents();
+    let injector = getTestBed();
+    location = injector.get(Location);
+    router = injector.get(Router);
   }));
+
+  beforeEach(
+    inject(
+      [Router, Location],
+      (_router: Router, _location: Location) => {
+        router = _router;
+        location = _location;
+      }
+    )
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -26,10 +62,20 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('MovieStar');
   });
 
-  it('should render title in a h1 tag', () => {
+  xit('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to MovieStar!');
   });
+
+  it('should go home', () => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    router.navigate([''])
+      .then(() => {
+        expect(location.path()).toBe('/');
+      })
+  });
 });
+
