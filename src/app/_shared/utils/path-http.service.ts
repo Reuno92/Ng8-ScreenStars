b555f8edd4d74a32602550591ec9f981e12c827d
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-18 19:39:49 +0100

    doc(PathHttpService): add explanation

--- src/app/_shared/utils/path-http.service.ts
+++ src/app/_shared/utils/path-http.service.ts
@@ -11,9 +11,9 @@ export class PathHttpService {
   /**
    * Help to create a path with api.
    * It's better one method than a multiple methods for each endpoint
-   * @param id  empty string by default
-   * @param suffix  same as id
-   * @param prefix  same as id
+   * @param id - empty string by default
+   * @param suffix - same as id, it's in front of because it's most used.
+   * @param prefix - same as id
    */
   public getPathTvShow(id = '', suffix = '', prefix = '') {
     return TV_SHOW + '/' + prefix + id + suffix + KEY;
