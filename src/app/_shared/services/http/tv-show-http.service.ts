d71df9aadee3020238392b60ea98b1949b4ed59e
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-18 19:12:59 +0100

    chore(tv-show-http-service): refactoring
    reduce import from models
    new method in replacement of all methods for return an just one httpRequest

--- src/app/_shared/services/http/tv-show-http.service.ts
+++ src/app/_shared/services/http/tv-show-http.service.ts
@@ -2,14 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {TV_SHOW, TVSHOWS_SUFFIX, TVSHOW_SUFFIX, KEY} from '../../../constant/api.constant';
-import {TV} from '../../models/TV/tv';
-import {Credit} from '../../models/TV/Credit';
-import {Review} from '../../models/TV/Review';
-import {Recommendation} from '../../models/TV/Recommendation';
-import {Similar} from '../../models/TV/Similar';
-import {Translation} from '../../models/TV/Translation';
-import {Rating} from '../../models/TV/Rating';
-import {Video} from '../../models/TV';
+import {TV, Credit, Review, Recommendation, Similar, Translation, Rating, Video } from '../../models/TV';
 
 @Injectable({
   providedIn: 'root'
@@ -31,66 +24,48 @@ export class TvShowHttpService {
   }
 
   public getCreditTvShow(id): Observable<Credit> {
-    return this.http.get<Credit>(this.getPathCreditTvShow(id), {headers: new HttpHeaders(), responseType: 'json'});
+    return this.http.get<Credit>(this.getPathTvShow(id, TVSHOW_SUFFIX.credits),
+      {headers: new HttpHeaders(), responseType: 'json'});
   }
 
   public getReviewTvShow(id): Observable<Review> {
-    return this.http.get<Review>(this.getPathReviewTvShow(id), );
+    return this.http.get<Review>(this.getPathTvShow(id, TVSHOW_SUFFIX.reviews),
+      {headers: new HttpHeaders(), responseType: 'json'});
   }
 
   public getRecommendations(id): Observable<Recommendation> {
-    return this.http.get<Recommendation>(this.getRecommendationPathTvShow(id), { headers: new HttpHeaders(), responseType: 'json'});
+    return this.http.get<Recommendation>(this.getPathTvShow(id, TVSHOW_SUFFIX.recommendations),
+      { headers: new HttpHeaders(), responseType: 'json'});
   }
 
   public getSimilarTVShow(id): Observable<Similar> {
-    return this.http.get<Similar>(this.getPathSimilarTvShow(id), {headers: new HttpHeaders(), responseType: 'json'});
+    return this.http.get<Similar>(this.getPathTvShow(id, TVSHOW_SUFFIX.similar),
+      {headers: new HttpHeaders(), responseType: 'json'});
   }
 
   public getContentTVShow(id): Observable<Rating> {
-    return this.http.get<Rating>(this.getPathContentRatingTvShow(id), {headers: new HttpHeaders(), responseType: 'json'})
+    return this.http.get<Rating>(this.getPathTvShow(id, TVSHOW_SUFFIX.content_rating),
+      {headers: new HttpHeaders(), responseType: 'json'})
   }
 
   public getTranslationsTVShow(id): Observable<Translation> {
-    return this.http.get<Translation>(this.getPathTranslationTvShow(id), {headers: new HttpHeaders(), responseType: 'json'})
+    return this.http.get<Translation>(this.getPathTvShow(id, TVSHOW_SUFFIX.translations),
+      {headers: new HttpHeaders(), responseType: 'json'})
   }
 
   public getVideosTVShow(id): Observable<Video> {
-    return this.http.get<Video>(this.getPathTvShowTabs(id, TVSHOW_SUFFIX.videos),{headers: new HttpHeaders(), responseType: 'json'})
+    return this.http.get<Video>(this.getPathTvShow(id, TVSHOW_SUFFIX.videos),
+      {headers: new HttpHeaders(), responseType: 'json'})
   }
 
-  private getPathTvShow(id): string {
-    return TV_SHOW + '/' + id + KEY;
-  }
-
-  private getPathTvShowTabs(id, suffix, prefix = '') {
+  /**
+   * Help to create a path with api.
+   * It's better one method than a multiple methods for each endpoint
+   * @param id
+   * @param suffix
+   * @param prefix empty string by default
+   */
+  private getPathTvShow(id, suffix = '', prefix = '') {
     return TV_SHOW + '/' + prefix + id + suffix + KEY;
-  }
-
-  private getPathCreditTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.credits + KEY;
-  }
-
-  private getPathReviewTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.reviews + KEY;
-  }
-
-  private getRecommendationPathTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.recommendations + KEY;
-  }
-
-  private getPathSimilarTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.similar + KEY;
-  }
-
-  private getPathContentRatingTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.content_rating + KEY;
-  }
-
-  private getPathTranslationTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.translations + KEY;
-  }
-
-  private getpathVideoTvShow(id): string {
-    return TV_SHOW + '/' + id + TVSHOW_SUFFIX.videos + KEY;
   }
 }
