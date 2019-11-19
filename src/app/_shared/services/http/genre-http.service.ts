48d8f5d36a557e17e0511c5193e22712966575a6
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-19 13:33:36 +0100

    style(genreHttpService): replace double quote by single quote

--- src/app/_shared/services/http/genre-http.service.ts
+++ src/app/_shared/services/http/genre-http.service.ts
@@ -12,10 +12,10 @@ export class GenreHttpService {
   constructor(private http: HttpClient) {}
 
   public getGenreMoviesList(): Observable<any> {
-    return this.http.get(MOVIES_LIST, {headers: new HttpHeaders(), responseType: "json"});
+    return this.http.get(MOVIES_LIST, {headers: new HttpHeaders(), responseType: 'json'});
   }
 
   public getGenreTvList(): Observable<any> {
-    return this.http.get(TV_LIST, {headers: new HttpHeaders(), responseType: "json"});
+    return this.http.get(TV_LIST, {headers: new HttpHeaders(), responseType: 'json'});
   }
 }
