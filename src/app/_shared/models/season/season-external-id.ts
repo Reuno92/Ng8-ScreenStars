eca1648d8af8cddc0f9b5caa61b1ace6c3c31a65
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-20 09:06:44 +0100

    fix(sesaonExternalId): pass properties in optional properties

--- src/app/_shared/models/season/season-external-id.ts
+++ src/app/_shared/models/season/season-external-id.ts
@@ -3,9 +3,9 @@ export class SeasonExternalId {
   constructor(
     public freebase_mid?: string,
     public freebase_id?: string,
-    public tvdb_id: string,
-    public tvrage_id: string,
-    public id: string
+    public tvdb_id?: string,
+    public tvrage_id?: string,
+    public id?: string
   ) {
   }
 }
